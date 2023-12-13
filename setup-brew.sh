71ea10ec08dfd0a26efdee15a0aa832c66567347
Jun Yamog <jkyamog@gmail.com>
2023-12-13 13:44:07 +1300

    updated configs files, some things in setup-macos.sh doesn't work but is still helpful to get most settings set. added vscode profile

--- setup-brew.sh
+++ setup-brew.sh
@@ -4,52 +4,53 @@ brew update
 brew upgrade
 
 # install brew-cask and java early, as dependencies on others
-brew install caskroom/cask/brew-cask
-
-brew tap homebrew/services
-
-brew tap tldr-pages/tldr
-brew install tldr
+brew services
 
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 brew install bash
 brew install bash-completion
-brew install zsh
+# brew install zsh # macos sanoma includes current version 
+brew install zsh-completions 
 
-brew install gnu-sed --with-default-names  # override built-in sed
+brew install gnu-sed # override built-in sed, fix on path
 
 # git
 brew install git
 brew install tig
 
 # jvm stuff
-brew cask install java
-brew install leiningen
-brew install maven
-brew install sbt
+# brew cask install java
+# brew install leiningen
+# brew install maven
+# brew install sbt
+
+# node
 
 # dev stuff
 # brew install mongodb
-brew install nginx
-brew install node
-brew install pcre             # perl regex
-brew install ssh-copy-id
-brew install tmux
-brew install reattach-to-user-namespace
+ #brew install nginx
+brew install npm
+# brew install pcre             # perl regex
+# brew install ssh-copy-id
+# brew install tmux
+# brew install reattach-to-user-namespace
+brew install python
 
 # useful utils
-brew install vim
-brew install z
+# brew install vim # macos sanoma includes current version
+# brew install z
+brew install bat
+brew install jq
+
+# some apps still need rosetta 2
+sudo softwareupdate --install-rosetta
 
 # useful user apps
-brew cask install dockertoolbox
-brew cask install google-chrome
-brew cask install virtualbox
-brew cask install dropbox
-brew cask install keepassx
-brew cask install vmware-horizon-client
-brew cask install flux
-brew cask install google-drive
+# brew install google-drive
+brew install visual-studio-code
+brew install lm-studio 
+brew install microsoft-edge
+brew install lg-onscreen-control
 
 # Remove outdated versions from the cellar.
 brew cleanup
